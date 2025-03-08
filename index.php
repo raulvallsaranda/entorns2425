dfdcb97c0882bf5cd9a59200c2648dfaffd92b0e
raulvallsaranda <115420946+raulvallsaranda@users.noreply.github.com>
2025-03-08 10:35:50 +0100

    Creem index.php: commit inicial

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ca">
+ <head>
+  <meta charset="utf-8">
+  <title>Entorns 24/25</title>
+  <link rel="stylesheet" href="estils.css">
+ </head>
+ <body>
+   <header>
+   <h1>Entorns 24/25</h1>
+   </header>
+   <main>
+     <h2>Treballant amb git</h2>
+   </main>
+   <footer>
+     <p>IES Lluís Simarro<br>1r DAM<br>Curs 24/25</p>
+   </footer>
+   
+ </body>
+</html>
